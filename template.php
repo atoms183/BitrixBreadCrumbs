8d1e9a1e93784d9e23b454af9a2f93bf1d1e6caa
Дмитрий Глазвин <sony183@bk.ru>
2016-12-28 00:02:24 +0300

    commit

---
+++ template.php
@@ -0,0 +1,62 @@
+<?php
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+/**
+ * @global CMain $APPLICATION
+ */
+
+global $APPLICATION;
+
+//delayed function must return a string
+if(empty($arResult))
+	return "";
+
+$strReturn = '';
+
+//we can't use $APPLICATION->SetAdditionalCSS() here because we are inside the buffered function GetNavChain()
+$css = $APPLICATION->GetCSSArray();
+if(!is_array($css) || !in_array("/bitrix/css/main/font-awesome.css", $css))
+{
+	$strReturn .= '<link href="'.CUtil::GetAdditionalFileURL("/bitrix/css/main/font-awesome.css").'" type="text/css" rel="stylesheet" />'."\n";
+}
+
+$strReturn .= '<div class="bx-breadcrumb">';
+
+$itemSize = count($arResult);
+for($index = 0; $index < $itemSize; $index++)
+{
+	$title = htmlspecialcharsex($arResult[$index]["TITLE"]);
+
+	$nextRef = ($index < $itemSize-2 && $arResult[$index+1]["LINK"] <> ""? ' itemref="bx_breadcrumb_'.($index+1).'"' : '');
+	$child = ($index > 0? ' itemprop="child"' : '');
+	$arrow = ($index > 0? '<i class="fa fa-angle-right"></i>' : '');
+
+	if($arResult[$index]["LINK"] <> "" && $index != $itemSize-1)
+	{
+		$strReturn .= '
+		<section class="page-title">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h4 class="text-uppercase">'.$title.'</h4>
+							<ol class="breadcrumb" id="bx_breadcrumb_'.$index.'"'.$child.$nextRef.'>
+								'.$arrow.'
+								<li><a href="'.$arResult[$index]["LINK"].'" title="'.$title.'
+									" itemprop="url">'.$title.'</a></li>';
+	}
+	else
+	{
+		$strReturn .= '
+								'.$arrow.'
+								<li><span itemprop="title">'.$title.'</span></li>
+							</ol>
+					</div>
+				</div>
+			</div>
+		</section>';
+	}
+}
+
+$strReturn .= '<div style="clear:both"></div></div>';
+
+return $strReturn;
